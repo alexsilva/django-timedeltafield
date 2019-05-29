2a3d65242de1c9320e6a6c4e804e2124003925bd
alexsilva <geniofuturo@gmail.com>
2019-05-29 17:57:11 -0300

    Installation of translations.

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         "timedelta",
         "timedelta.templatetags",
     ],
+    include_package_data=True,
     package_data={'timedelta': ['VERSION']},
     classifiers=[
         'Framework :: Django :: 1.4',
